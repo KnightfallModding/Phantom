1e9d9a159b4c9b748bebcc4f8164408f8c9cd84d
Aeryle <contact.aeryle@pm.me>
2025-08-06 02:58:15 +0200

    :wrench: improve LF conversion

--- Plugin/PluginInfo.cs
+++
@@ -1,6 +0,0 @@
-namespace Phantom;
-public static class PluginInfo {
-  public const string ID = "com.aeryle.phantom";
-  public const string NAME = "Phantom";
-  public const string VERSION = "0.1.0-alpha";
-}
