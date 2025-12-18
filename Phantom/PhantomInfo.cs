19adf5bc8fb3868bd28f39a2883e90194c7db7b1
Aeryle <contact.aeryle@pm.me>
2025-12-18 20:35:57 +0100

    :tada: first commit

---
+++ Phantom/PhantomInfo.cs
@@ -0,0 +1,10 @@
+namespace Phantom;
+
+public static class PhantomInfo
+{
+    public const string Id = "com.aeryle.phantom.debug";
+    public const string Name = "Phantom";
+    public const string Description = "Phantom is a Knightfall mod that extends the abilities of the game past their limits";
+    public const string Version = "0.0.1";
+    public const string Author = "Aeryle";
+}
---
+++ Phantom/PhantomPlugin.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using Phantom;
+
+[assembly: MelonGame("Landfall Games", "Knightfall")]
+[assembly: MelonInfo(
+    typeof(PhantomPlugin),
+    PhantomInfo.Name,
+    PhantomInfo.Version,
+    PhantomInfo.Author
+)]
+[assembly: MelonColor(1, 255, 102, 99)]
+
+namespace Phantom;
+
+public class PhantomPlugin : MelonPlugin
+{
+    // ReSharper disable once NullableWarningSuppressionIsUsed
+    public MelonLogger.Instance Logger = null!;
+
+    public override void OnInitializeMelon()
+    {
+        Logger = LoggerInstance;
+        Logger.Msg("Initializing Phantom Plugin");
+    }
+}
